5d4fb765d410b19bbc2d8bb775e37d5490fac890
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-04-06 10:23:21 -0400

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   wrap_parameters format: []
 
   def index
@@ -6,14 +7,20 @@ class ToysController < ApplicationController
     render json: toys
   end
 
+  def show 
+    toy = Toy.find(params[:id])
+    render json: toy
+  end
+
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -26,6 +33,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_unprocessable_entity_response
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
